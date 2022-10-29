65573651c52ed6dad5450af9bae2c2a45bb23d36
madcore <madcored@gmail.com>
2022-10-29 15:39:09 +0300

    test1

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,57 @@
+import java.util.List;
 import java.util.regex.*;
 import java.util.Arrays;
-import java.io.Console;
+import java.util.Scanner;
 
-public class Main {
+class Main {
 
-    private static final String[] rNumbs = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-
+    private static final List<String> rNumbs = Arrays.asList("error", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X");
     private static int rome2num(String rNum) { // 1 to 10
-        return (Arrays.asList(rNumbs).indexOf(rNum)+1);
+        return rNumbs.indexOf(rNum);
     }
 
-    private static String num2rome(int num) {
+    private static String num2rome(int num) { // 1 to 399
         if (num <= 0) throw new NullPointerException("Invalid Rome Number Result");
         StringBuilder res = new StringBuilder();
         while (num >= 100) {
-            res.append("C");
-            num -= 100;
+            res.append("C"); num -= 100;
         }
         if (num >= 90) {
-            res.append("XC");
-            num -= 90;
+            res.append("XC"); num -= 90;
         } else if (num >= 50) {
-            res.append("L");
-            num -= 50;
+            res.append("L"); num -= 50;
         } else if (num >= 40) {
-            res.append("XL");
-            num -= 40;
+            res.append("XL"); num -= 40;
         }
         while (num >= 10) {
-            res.append("X");
-            num -= 10;
+            res.append("X"); num -= 10;
         }
-        if (num > 0) res.append(rNumbs[num - 1]);
+        if (num > 0) res.append(rNumbs.get(num));
         return res.toString();
     }
 
     private static int calcExpr(int a, int b, String op) {
         return switch (op) {
-            case ("+") -> a + b;
-            case ("-") -> a - b;
             case ("*") -> a * b;
             case ("/") -> a / b;
-            default -> -1;
+            case ("-") -> a - b;
+            default -> a + b; // op can be passed only +-/*
         };
     }
 
     private static final Pattern patternA = Pattern.compile("^([1-9]|10)\\s*([*+-/])\\s*([1-9]|10)$");
     private static final Pattern patternR = Pattern.compile("^(X|IX|IV|V?I{0,3})\\s*([*+-/])\\s*(X|IX|IV|V?I{0,3})$");
 
-    public static String calc (String expr) {
-        Matcher matcherA = patternA.matcher(expr);
-        Matcher matcherR = patternR.matcher(expr);
+    public static String calc (String input) {
+        Matcher matcherA = patternA.matcher(input);
+        Matcher matcherR = patternR.matcher(input);
         if (matcherA.find()) {
             return Integer.toString(calcExpr(
-                    Integer.parseInt(matcherA.group(1)),
-                    Integer.parseInt(matcherA.group(3)),
+                        Integer.parseInt(matcherA.group(1)),
+                        Integer.parseInt(matcherA.group(3)),
                         matcherA.group(2)
                     ));
         } else if (matcherR.find()) {
-
             return num2rome(calcExpr(
                         rome2num(matcherR.group(1)),
                         rome2num(matcherR.group(3)),
@@ -66,6 +59,21 @@ public class Main {
                     ));
         } else {
             throw new NullPointerException("Invalid Expression");
+        }
+    }
+
+    public static void main(String[] args) { //for testing
+        for (int i =1;i<=100;i++)
+            System.out.println(i + "\t" +
+                    (i <= 10 ? rome2num(num2rome(i)) : "-") +
+                    "\t"  + num2rome(i)
+            );
+        Scanner myInput = new Scanner( System.in );
+        while (true) {
+            System.out.println("Enter Expression:");
+            String expr = myInput.nextLine().toUpperCase();
+            if (expr.isEmpty()) return;
+            System.out.println("Result: " + calc(expr));
         }
     }
 }
